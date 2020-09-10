8d6cf9851827a75a6e0d38c6f37a9ee6ad463b48
IlyaToritsyn <toritsyn41@gmail.com>
2020-09-10 23:46:59 +0300

    Get project independence.

--- CS_1_Quadratic_equation_WPF/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ CS_1_Quadratic_equation_WPF/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -109,7 +109,7 @@ namespace CS_1_Quadratic_equation_WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CS_1_Quadratic_equation_WPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CS_1_Quadratic_equation_WPF;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
